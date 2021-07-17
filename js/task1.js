cf818eaf1ba1d702de6aa92d8116db1bc51fa724
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-07-17 08:41:44 +0300

    corrections

--- js/task1.js
+++ js/task1.js
@@ -5,5 +5,6 @@ const headersTextContent = [...categoriesListItemElement].map(el => {
   return `Категория: ${el.querySelector('h2').textContent}, количество элементов: ${el.lastElementChild.childElementCount}`;
 });
 
-console.log('количество категорий в ul#categories :>> ', categoriesListElement.childElementCount);
+
+console.log(`В списке ${categoriesListElement.childElementCount} категории.`);
 console.log( headersTextContent.join( '; \n' ));
